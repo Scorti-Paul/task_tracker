bb458c23be495fb6072209c8b9615cfd5cd55920
Paul Scorti <scortip@gmail.com>
2020-02-26 17:52:41 +0000

    Add a Form part

---
+++ public/js/dist/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('.add-circle').on('click', function() {
+		$('.task-form').addClass('active');
+	});
+
+	$('.task-form .close-btn').on('click', function() {
+		$('.task-form').removeClass('active');
+	});
+});
